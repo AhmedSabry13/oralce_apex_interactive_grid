d776c5b1f929683761acee1292d8c96cabd938a8
AhmedSabry13 <40600333+AhmedSabry13@users.noreply.github.com>
2026-05-16 22:22:54 +0300

    Add AJAX callback process 'SAVE_EMP' for employee data
    Implement AJAX callback process to save employee data from JSON input.

---
+++ PL_SQL_process.sql
@@ -0,0 +1,33 @@
+-- THE AJAX CALLBACK PROCESS NAMED "SAVE_EMP"
+DECLARE
+    l_clob CLOB;
+    CURSOR JSD(P_CLOB_C CLOB) IS SELECT jt.*
+FROM JSON_TABLE(
+    ( P_CLOB_C),
+    '$[*]'
+    COLUMNS (
+        ACTION char PATH '$.ACTION',
+        C1 number PATH '$.C1',
+        C2 NUMBER PATH '$.C2',
+        C3   NUMBER PATH '$.C3'
+    )
+) jt
+order by invoice_id
+BEGIN
+    l_clob := apex_application.g_clob_01;
+    -- just test receive
+FOR I IN JSD(l_clob) LOOP
+-- YOUR CODE HERE
+  null;
+END LOOP;
+    COMMIT;
+    apex_json.open_object;
+    apex_json.write('status', 'success');
+    apex_json.close_object;
+EXCEPTION
+    WHEN OTHERS THEN
+        apex_json.open_object;
+        apex_json.write('status', 'fail');
+        apex_json.write('error', SQLERRM);
+        apex_json.close_object;
+END;
